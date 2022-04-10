990c31cee296acb7aff5267712175bd11adfe41f
ichbingrg <65743692+ichbingrg@users.noreply.github.com>
2022-04-10 15:34:56 +0200

    add source files

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,62 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank{
+
+  //stable keyword makes a variable orthogonally persistant which means the changes on the value remains even if the program is rerun
+  stable var currentValue : Float = 300;
+  currentValue := 100;
+  Debug.print(debug_show(currentValue));
+
+
+  stable var startTime = Time.now();
+  startTime := Time.now();
+  Debug.print(debug_show(startTime)); //shows currenttime in nanoseconds
+
+  //Debug.print : (Text) -> (output in terminal) but it only accepts texts => wrapping needed
+  //debug_show : () -> (Text)
+  // Debug.print(debug_show(currentValue)); // prints currentValue in the terminal
+
+  //let works as constant
+  //let id = 342354242343245345;
+
+  //Debug.print(debug_show(debug_show(id)));
+
+  public func topUp(amount: Float){
+    currentValue+=amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+
+
+
+  public func withdraw(amount: Float){
+    let remainingValue: Float = currentValue-amount;
+    if(remainingValue>=0){
+      currentValue -= amount;
+      Debug.print(debug_show(currentValue));
+    }else{
+      Debug.print("Not enough funds");
+    }
+  };
+
+  //topUp();
+
+  //update needs to go through the blockchain and require consensus from each node in the blcokchain that's why it is slow 
+  //query - READ ONLY - no update to anything but just fetching the values. -> faster than update
+  // <public> query func <funcname>(attributes): async <returnType> { ---- code ----- };
+  public query func checkBalance(): async Float {
+
+    return currentValue;
+
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime-startTime;
+    let timeElapsedS = timeElapsedNS/1000000000;
+    currentValue := currentValue*((1.01)** Float.fromInt(timeElapsedS)); 
+    startTime := currentTime;
+  };
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,47 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async ()=>{
+  console.log("loaded");
+  update();
 });
+
+document.querySelector("form").addEventListener("submit", async (event)=>{
+  event.preventDefault();
+  const button = event.target.querySelector("#submit-btn");
+
+  console.log("submitted");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const withdrawalAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true); //once the button is pressed disable it to prevent users from clicking it multiple times
+  //top ups and withdrawal are updates on the DS thus will take a while to finish.
+  console.log("Topup amount : " + inputAmount);
+  console.log("withdrawal amount : " + withdrawalAmount);
+
+  if((document.getElementById("input-amount").value)){
+    await dbank.topUp(inputAmount);
+  }
+
+  if((document.getElementById("withdrawal-amount").value)){
+    const balance = await dbank.checkBalance();
+    if(withdrawalAmount>balance){
+      console.log("not enough funds");
+    }else{
+      await dbank.withdraw(withdrawalAmount);
+    }
+  }
+
+  // dbank.compound(); // to enable compound interest, but the compount interest is set to 1% per second for experimental reasons, therefore might result in astronomical numbers
+  
+  update();
+  button.removeAttribute("disabled"); // button enabled again after the update has finished
+})
+
+async function update(){
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount*100) / 100;
+  document.getElementById("input-amount").value="" ; //empty the textfield after the update
+  document.getElementById("withdrawal-amount").value="" ; //empty the textfield after the update
+  
+}
